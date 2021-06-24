4f1da4ffae533a925f8749a3e04a104b7cdc72fb
jrobilee <jrobilee@amazon.com>
2021-06-24 10:37:57 -0400

    Form handling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
 
 function App() {
+  const [state, setState] = useState({
+    fname: "",
+    lname: "",
+  })
+
+  const handleChange = e => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = e =>{
+    
+    e.preventDefault();
+    //alert("You are submitting " + state.fname + " " + state.lname);
+
+    var myHeaders = new Headers();
+
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({"firstName":state.fname ,"lastName":state.lname});
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    fetch("https://cyvx06f8jk.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
+    .then(response => response.text())
+    .then(result => alert(JSON.parse(result).body))
+    .catch(error => console.log('error', error));
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Form Handling</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name:{" "}
+          <input
+            type="text"
+            name="fname"
+            value={state.fname}
+            onChange={handleChange}
+          />
+        </label>{" "}
+        <label>
+          Last Name:{" "}
+          <input
+            type="text"
+            name="lname"
+            value={state.lname}
+            onChange={handleChange}
+          />
+        </label>
+        <button>Submit!</button>
+      </form>
+      <h5>
+        Name: {state.fname} {state.lname}
+      </h5>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
